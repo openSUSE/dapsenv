29f4797513a826a7db28a19f60ae34b21714d27e
Manuel Schnitzer <mschnitzer@suse.de>
2016-03-29 12:19:55 +0200

    Add command line interface

---
+++ source/dapsenv/argparser.py
@@ -0,0 +1,62 @@
+#
+# Copyright (c) 2016 SUSE Linux GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+
+import argparse
+from dapsenv.general import __version__
+
+class ArgParser:
+
+    def __init__(self, args):
+        self.args = args
+        self.parser = argparse.ArgumentParser(
+            description="A build environment to build documentations with DAPS."
+        )
+        self.cmdSubParser = self.parser.add_subparsers(
+            dest="action",
+            title="Available subcommands",
+        )
+
+        self.addGlobalOptions()
+        self.addConfigCommand()
+
+    def print_help(self):
+        self.parser.print_help()
+
+    def parse(self):
+        return vars(self.parser.parse_args(args=self.args))
+
+    def addGlobalOptions(self):
+        self.parser.add_argument(
+            "--version", action="version", help="Displays the current version of this program.",
+            version="DAPS Build Environment Version {}".format(__version__)
+        )
+
+    def addConfigCommand(self):
+        cmd = self.cmdSubParser.add_parser(
+            "config", aliases=["c"], help="Modifying the configuration file"
+        )
+
+        cmd.add_argument(
+            "--property", "-p", action="store", required=True,
+            help="Specifies a property from the config file. If no value is set with --value, " \
+            "only the current value of the property will be print on the command line."
+        )
+
+        cmd.add_argument(
+            "--value", action="store", help="Sets a new value for a property."
+        )
