55c57f1c524cd3ccc7d679998592512646fd3392
Manuel Schnitzer <mschnitzer@suse.de>
2016-05-17 14:47:51 +0200

    Fix $ROOTID issue in build.sh

--- source/dapsenv/data/build.sh
+++ source/dapsenv/data/build.sh
@@ -5,9 +5,16 @@ STATUS_FILE="/tmp/build_status"
 daps -vv -d /tmp/build/$4/$1 $2 &> $BUILD_LOG
 
 if [ $? -eq 0 ]; then
+  source /tmp/build/$4/$1
+
   ARCHIVE_NAME="documentation.tar"
   BUILD_DIR_NAME=$(expr "$1" : '^DC\-\(.*\)$')
-  BUILD_DIR_PATH="$3/build/$BUILD_DIR_NAME/$2/$BUILD_DIR_NAME"
+  if [ "$ROOTID" -eq "" ]; then
+    BUILD_DIR_PATH="$3/build/$BUILD_DIR_NAME/$2/$BUILD_DIR_NAME"
+  else
+    BUILD_DIR_PATH="$3/build/$BUILD_DIR_NAME/$2/$ROOTID"
+  fi
+
   cd $BUILD_DIR_PATH
   tar cfv $ARCHIVE_NAME * > /dev/null
   mv $ARCHIVE_NAME /tmp
